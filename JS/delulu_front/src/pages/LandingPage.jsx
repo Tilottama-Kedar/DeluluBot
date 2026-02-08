870f9e129c7dca0e2fa54f6d226cb31434a7c1f7
Tilottama-Kedar <kedartilottama2003@gmail.com>
2026-02-09 04:27:54 +0530

    Added center bot zoom animation and soft lightning background effect

--- JS/delulu_front/src/pages/LandingPage.jsx
+++ JS/delulu_front/src/pages/LandingPage.jsx
@@ -4,7 +4,13 @@ import logo from "../assets/logo.png";
 function LandingPage() {
   return (
     <div className="landing">
+      {/* Original top-left logo (unchanged) */}
       <img src={logo} alt="Logo" className="landing-logo" />
+
+      {/* Center zoom-in bot */}
+      <div className="bot-wrapper">
+        <img src={logo} alt="Bot" className="bot-zoom" />
+      </div>
     </div>
   );
 }
