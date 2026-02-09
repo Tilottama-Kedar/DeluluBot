b2bcc0e1e1ace4e163c9e60a853a065bb18ce644
curfewUnzipper <cryingpushpa@gmail.com>
2026-02-09 22:59:55 +0530

    gg3

--- JS/delulu_front/src/App.jsx
+++ JS/delulu_front/src/App.jsx
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-import './App.css'
 
-function App() {
- 
-
-  return (
-    <>
-        <p>
-         HI
-        </p>
-    
-    </>
-  )
-}
-
-export default App
-=======
 import LandingPage from "./pages/LandingPage";
 
 function App() {
@@ -23,4 +6,3 @@ function App() {
 }
 
 export default App;
->>>>>>> a6a982654da3b4bfe1974d2cf90baf25d375375b
