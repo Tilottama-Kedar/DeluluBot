ee663d5882ff27d194bb0ed6152bb343f2b7b6c3
Tilottama-Kedar <kedartilottama2003@gmail.com>
2026-02-10 01:47:31 +0530

    Redirecting to the home page y swiping up landing page

--- JS/delulu_front/src/App.jsx
+++ JS/delulu_front/src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
 
 function App() {
-  return <LandingPage />;
+  const [page, setPage] = useState("landing");
+
+  return (
+    <>
+      {page === "landing" && <LandingPage onSwipeUp={() => setPage("home")} />}
+      {page === "home" && <Home />}
+    </>
+  );
 }
 
 export default App;
